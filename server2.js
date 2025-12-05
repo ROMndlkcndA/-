574092c022e54945172a975543ae98e075b2cceb
HollyRoot <132997782+ROMndlkcndA@users.noreply.github.com>
2025-12-05 16:40:32 +0200

    Update server2.js

--- server2.js
+++ server2.js
@@ -431,6 +431,8 @@ Developed by: HollyRoot
 // ------------------------------
 //   SERVER LISTEN
 // ------------------------------
-server.listen(3000, () => {
-    console.log('C&C Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`C&C Server is running on port ${PORT}`);
 });
